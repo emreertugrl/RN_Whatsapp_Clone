15893c4c9ab7f704592d672c88d48e47133be46e
emreertugrl <emrertugrl@gmail.com>
2025-05-05 23:24:49 +0300

    status bar changed

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import RootNavigator from './src/router/rootNavigation';
+import {StatusBar} from 'react-native';
 const App: React.FC = () => {
   return (
     <NavigationContainer>
+      <StatusBar backgroundColor={'white'} barStyle={'dark-content'} />
       <RootNavigator />
     </NavigationContainer>
   );
