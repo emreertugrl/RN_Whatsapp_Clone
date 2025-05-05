9a9997a584fae5b0e5ebae66544c469b75398622
emreertugrl <emrertugrl@gmail.com>
2025-05-05 19:41:21 +0300

    default style defined , color defined

---
+++ src/styles/defaultScreenStyle.ts
@@ -0,0 +1,13 @@
+import {StyleSheet} from 'react-native';
+import Colors from '../utils/colors';
+
+const defaultScreenStyle = StyleSheet.create({
+  safeArea: {flex: 1},
+  container: {
+    flex: 1,
+    padding: 10,
+    backgroundColor: Colors.WHITE,
+  },
+});
+
+export default defaultScreenStyle;
---
+++ src/utils/colors.ts
@@ -0,0 +1,9 @@
+const enum Colors {
+  GRAY_1 = '#545458',
+  GRAY_2 = '#979797',
+  GRAY_3 = '#F6F6F5',
+  BLUE_1 = '#007AFF',
+  WHITE = '#FFFFFF',
+}
+
+export default Colors;
