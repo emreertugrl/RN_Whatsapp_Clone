487f63055682102aba64e66a42a8f37d1a0ebe01
emreertugrl <emrertugrl@gmail.com>
2025-05-06 21:05:55 +0300

    contact header design completed

--- src/components/router/contactHeader.tsx
+++ src/components/router/contactHeader.tsx
@@ -1,25 +1,76 @@
-//import liraries
-import React, {Component} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React from 'react';
+import {Text, StyleSheet, View, TouchableOpacity} from 'react-native';
+import {ContactProps} from '../../modals/data/contacts/contactTypes';
+import {SafeAreaView} from 'react-native';
+import Colors from '../../utils/colors';
+import {ArrowLeft, Call, Video} from 'iconsax-react-nativejs';
+import {useNavigation} from '@react-navigation/native';
 
-// create a component
-const ContactHeader = () => {
+const ContactHeader: React.FC<ContactProps> = ({contact}) => {
+  const navigation = useNavigation();
   return (
-    <View style={styles.container}>
-      <Text>ContactHeader</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <TouchableOpacity onPress={() => navigation.goBack()}>
+        <ArrowLeft size={28} />
+      </TouchableOpacity>
+      <View style={styles.imageContaine}>
+        <Text style={styles.imageText}>
+          {contact?.familyName[0]}
+          {contact?.givenName[0]}
+        </Text>
+      </View>
+      <View style={styles.nameContainer}>
+        <Text style={{fontSize: 18, fontWeight: 'bold', color: Colors.BLACK}}>
+          {contact?.familyName}
+
+          {' ' + contact?.givenName}
+        </Text>
+        <Text style={{fontSize: 18, color: Colors.GRAY_2}}>
+          {contact?.phoneNumbers[0].number}
+        </Text>
+      </View>
+      <View style={styles.iconContainer}>
+        <TouchableOpacity>
+          <Video color={Colors.BLUE_1} size={30} />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Call color={Colors.BLUE_1} size={30} />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 };
 
-// define your styles
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: Colors.WHITE,
+    paddingTop: 50,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    gap: 20,
+    padding: 10,
+  },
+  nameContainer: {
     flex: 1,
+  },
+  imageContaine: {
+    backgroundColor: Colors.GRAY_3,
+    borderWidth: 1,
+    width: 50,
+    height: 50,
+    borderRadius: 50,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#2c3e50',
+  },
+  imageText: {
+    fontSize: 30,
+  },
+  iconContainer: {
+    flexDirection: 'row',
+    gap: 10,
+    alignItems: 'center',
   },
 });
 
-//make this component available to the app
 export default ContactHeader;
