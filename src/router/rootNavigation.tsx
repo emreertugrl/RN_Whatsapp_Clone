24feaa8560e46925d3e81943d4a82e71eceafc47
emreertugrl <emrertugrl@gmail.com>
2025-05-08 15:31:07 +0300

    last seen for AppState defined

--- src/router/rootNavigation.tsx
+++ src/router/rootNavigation.tsx
@@ -15,75 +15,68 @@ import {useAppSelector} from '../store/hooks';
 
 const Stack = createNativeStackNavigator();
 const RootNavigator = () => {
-  const {phoneNumber, selectedCountry} = useAppSelector(state => state.auth);
+  const {phoneNumber} = useAppSelector(state => state.auth);
+  const phone = phoneNumber;
   useEffect(() => {
-    const phone = phoneNumber;
-    const subscription = AppState.addEventListener(
+    const subscribe = AppState.addEventListener(
       'change',
-      async (nextAppState: string) => {
-        const isOnline = nextAppState === APPSTATE.ACTIVE;
-        console.log(
-          'App state değişti:',
-          nextAppState,
-          '=> isOnline:',
-          isOnline,
-        );
-        try {
-          await firestore()
+      async (nextAppState: any) => {
+        if (nextAppState == APPSTATE.ACTIVE) {
+          firestore().collection('Users').doc(phone).update({
+            isOnline: true,
+            lastSeen: firestore.FieldValue.serverTimestamp(),
+          });
+        } else {
+          firestore()
             .collection('Users')
             .doc(phone)
-            .set({isOnline}, {merge: true}); // ✅ güvenli çözüm
-        } catch (error) {
-          console.error('Firestore güncelleme hatası:', error);
+            .update({
+              isOnline: false,
+              lastSeen: firestore.FieldValue.serverTimestamp(),
+            });
         }
       },
     );
-
-    return () => {
-      subscription.remove();
-    };
-  }, [phoneNumber, selectedCountry]);
+  }, []);
 
   return (
-    <>
-      <Stack.Navigator
-      // initialRouteName={Routes.TAB}
-      >
-        <Stack.Screen
-          options={({navigation}) => ({
-            headerRight: () => (
-              <Pressable
-                onPress={() => navigation.navigate(Routes.TAB)}
-                style={{width: 80}}>
-                <Text style={{color: Colors.BLUE_1, fontSize: 18}}>Done</Text>
-              </Pressable>
-            ),
-          })}
-          name={Routes.PHONENUMBER}
-          component={PhoneNumber}
-        />
-        <Stack.Screen name={Routes.COUNTRYCODE} component={CountryCode} />
-        <Stack.Screen
-          name={Routes.USERREGISTIRETIONINFO}
-          component={UserRegistirationInfo}
-        />
-        <Stack.Screen
-          options={{
-            headerShown: false,
-          }}
-          name={Routes.TAB}
-          component={TabNavigator}
-        />
-        <Stack.Screen name={Routes.CONTACTS} component={Contacts} />
-        <Stack.Screen
-          options={{
-            headerShown: false,
-          }}
-          name={Routes.MESSAGES}
-          component={Messages}
-        />
-      </Stack.Navigator>
-    </>
+    <Stack.Navigator
+    // initialRouteName={Routes.TAB}
+    >
+      <Stack.Screen
+        options={({navigation}) => ({
+          headerRight: () => (
+            <Pressable
+              onPress={() => navigation.navigate(Routes.TAB)}
+              style={{width: 80}}>
+              <Text style={{color: Colors.BLUE_1, fontSize: 18}}>Done</Text>
+            </Pressable>
+          ),
+        })}
+        name={Routes.PHONENUMBER}
+        component={PhoneNumber}
+      />
+      <Stack.Screen name={Routes.COUNTRYCODE} component={CountryCode} />
+      <Stack.Screen
+        name={Routes.USERREGISTIRETIONINFO}
+        component={UserRegistirationInfo}
+      />
+      <Stack.Screen
+        options={{
+          headerShown: false,
+        }}
+        name={Routes.TAB}
+        component={TabNavigator}
+      />
+      <Stack.Screen name={Routes.CONTACTS} component={Contacts} />
+      <Stack.Screen
+        options={{
+          headerShown: false,
+        }}
+        name={Routes.MESSAGES}
+        component={Messages}
+      />
+    </Stack.Navigator>
   );
 };
 
