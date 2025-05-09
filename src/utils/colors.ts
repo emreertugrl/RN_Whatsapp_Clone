a187de320c1d80d06ca8136840f2a84cd7d27745
emreertugrl <emrertugrl@gmail.com>
2025-05-09 18:26:05 +0300

    colors defined

--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -3,8 +3,17 @@ const enum Colors {
   GRAY_2 = '#979797',
   GRAY_3 = '#F6F6F5',
   BLUE_1 = '#007AFF',
+  BLUE_2 = '#397AFE',
   WHITE = '#FFFFFF',
   BLACK = '#000000',
+  BLACK_2 = '#222',
+  GREEN_1 = '#DCF7C5',
+  GREEN_2 = '#25cf43',
+  GREEN_3 = '#4BD763',
+  GREEN_4 = '#07AD9F',
+  YELLOW_1 = '#FDF6C4',
+  YELLOW_2 = '#FBB500',
+  RED_1 = '#FF3B2F',
 }
 
 export default Colors;
